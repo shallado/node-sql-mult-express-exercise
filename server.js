84482afadc5169da1708e5aeaadd8d9e2e458fd7
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-22 21:16:10 -0700

    Configure server connection

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () => console.log(
+  `Successfully connected to the database on port: ${port}`
+));
