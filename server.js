9c72e77fd86709185101adc2fae08532a3601283
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-23 11:19:12 -0700

    Configure react to handle all backend routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require('express');
 const { sequelize } = require('./models');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const publicPath = path.join(__dirname, 'client', 'build');
+app.use(express.static(publicPath));
 
 sequelize.sync({ force: true })
-  .then(() => console.log('Successfully synced models to the database'))
+  .then(() => {
+    console.log('Successfully synced models to the database');
+    app.get('*', (req, res) => res.sendFile(`${publicPath}/index.html`));
+  })
   .catch((err) => console.log(
     'Unable to sync models to the database: \n', 
     err
